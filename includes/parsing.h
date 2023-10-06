8e08f5db9b5db3db504f33e0f3afebba987a998f
xhsle <mhammedboukelalen@gmail.com>
2023-10-06 03:52:41 +0000

    text_path

--- includes/parsing.h
+++ includes/parsing.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: niboukha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: xshel <xshel@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/03 12:58:47 by m-boukel          #+#    #+#             */
-/*   Updated: 2023/10/05 15:39:13 by niboukha         ###   ########.fr       */
+/*   Updated: 2023/10/06 03:44:30 by xshel            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,6 +48,7 @@ typedef struct files
 {
 	char	**file;
 	char	*d;
+	char	**d_spl;
 	char	*clr;
 	char	*map;
 	char	**map_spl;
@@ -73,5 +74,6 @@ int	check_Deriction(t_data *data);
 int	check_colors(t_data *data);
 int	ft_new_atoi(char *str);
 void	check_player_pos(t_data *data);
+int    check_texture_path(t_data *data);
 
 #endif
---
+++ src/parsing/textutre_path.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textutre_path.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xshel <xshel@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 21:06:36 by xshel             #+#    #+#             */
+/*   Updated: 2023/10/06 03:50:40 by xshel            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../incliudes/cub3d.h"
+
+
+int    check_texture_path(t_data *data)
+{
+    int i;
+
+    i = 0;
+    char **tmp;
+
+    tmp = NULL;
+    data->files->d_spl = ft_split(data->files->d, '\n');
+    while (data->files->d_spl[i])
+    {
+        tmp = ft_split(data->files->d_spl[i], ' ');
+        if (ft_strcmp(tmp[0], "NO") == 0 || ft_strcmp(tmp[0], "SO") == 0 ||
+                    ft_strcmp(tmp[0], "WE") == 0 || ft_strcmp(tmp[0], "EA") == 0)
+        {
+            if (open(tmp[1], O_RDONLY))
+                return 1;
+            else
+                return 0;    
+        }
+        free(tmp[0]);
+        free(tmp[1]);
+        free(tmp);
+        i++;
+    }
+    return 0;
+}
